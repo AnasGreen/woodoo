485b44c4caba67b69e7f337920b9505bfc535fd3
Anas LAAJILY <anaslaajily@gmail.com>
2026-08-21 12:31:42 +0100

    ci(woodoo): align Symfony test container for PHPStan

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -6,7 +6,7 @@ use App\Kernel;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-$kernel = new Kernel('dev', true);
+$kernel = new Kernel('test', true);
 $kernel->boot();
 
 return $kernel->getContainer()->get('doctrine')->getManager();
